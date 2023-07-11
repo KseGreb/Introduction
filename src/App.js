81e0aee570815313fb6d3fb3c9ee57aa4b97e7b3
“KseGreb” <ksegreb@gmail.com>
2023-07-11 14:08:22 -0600

    modifies app css check it out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import newFile from './newFile'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React!!! It's fun!!!
         </a>
       </header>
+      <newFile.js/>
     </div>
   );
 }

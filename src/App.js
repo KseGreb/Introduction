eb4dab5d07b9bf56ad8984882688fae05f30fd8f
“KseGreb” <ksegreb@gmail.com>
2023-07-12 08:50:49 -0600

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          still trying
+          Let's try and push it
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!! It's fun!!!
+          Learn React with me
         </a>
       </header>
       <newFile.js/>
